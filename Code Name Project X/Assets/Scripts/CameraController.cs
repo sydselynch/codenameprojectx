96ae07e9da9a1c437930a1264560d0e4da1217cd
Syd <slynch2@uoregon.edu>
2018-10-01 18:53:02 -0700

    init commit

---
+++ Code Name Project X/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public float distance;
+    
+    Vector3 offset = new Vector3(0, 0.3f, 0);
+
+    void Start()
+    {
+        if (Display.displays.Length > 1)
+            Display.displays[2].Activate();
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position - (player.transform.forward * distance) + offset;
+        transform.LookAt(player.transform);
+        transform.position += offset;
+    }
+}
---
+++ Code Name Project X/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float speed = 10;
+	public float strafeSpeed = 0.3f;
+    public float sensitivity = 1;
+    public float maxSpeed = 20;
+    public float minSpeed = 0;
+    private float totalTime;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+		StrafeUpdate();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "block")
+        {
+            
+
+        }
+
+    }
+
+	void StrafeUpdate()
+	{
+		if (Input.GetKey (KeyCode.A)) {
+			Vector3 position = rb.transform.position;
+			position.x -= strafeSpeed;
+			rb.transform.position = position;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			Vector3 position = rb.transform.position;
+			position.x += strafeSpeed;
+			rb.transform.position = position;
+		}
+	}
+}
